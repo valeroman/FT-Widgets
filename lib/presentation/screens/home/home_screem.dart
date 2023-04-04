4b0882f526a557cadc617a8d14514539eeb28e37
valeroman <valeroman@gmail.com>
2023-04-04 16:34:27 -0500

    HomeScreen creado

---
+++ lib/presentation/screens/home/home_screem.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreem extends StatelessWidget {
+  const HomeScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter + Material 3'),
+      ),
+    );
+  }
+}
