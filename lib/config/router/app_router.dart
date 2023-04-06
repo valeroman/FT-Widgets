6d48036ae5c4743b87189c885de58e345a7b69e9
valeroman <valeroman@gmail.com>
2023-04-05 21:41:03 -0500

    animatedContainer terminado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -39,5 +39,11 @@ final appRouter = GoRouter(
       builder: (context, state) => const SnackbarScreen(),
     ),
 
+    GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+
   ],
 );
